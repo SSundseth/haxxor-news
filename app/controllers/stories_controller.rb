f110a795944eab6ad0ef8bf8e8e1ef1386eb0d0d
Stefan Sundseth <stefan.sundseth@viget.com>
2012-06-13 14:42:44 -0400

    added page to submit links

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,18 @@
 class StoriesController < ApplicationController
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(params[:story])
+    if @story.save
+      redirect_to @story
+    else
+      render 'new'
+    end
+  end
+             
   def index 
     @stories = Story.all
   end
