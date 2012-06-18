3da5be5016ce9b7b340e2bf59b71330ac26a7abb
Stefan Sundseth <stefan.sundseth@viget.com>
2012-06-18 09:14:56 -0400

    added custom date format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 HaxxorNews::Application.initialize!
+
+[Date, Time, DateTime].each {|klass| klass::DATE_FORMATS[:custom_date] = "%D at %l:%M%p"}
