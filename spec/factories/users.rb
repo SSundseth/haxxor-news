acfb16b29792b2f4874d37cb8cf641dca42801c6
Stefan Sundseth <stefan.sundseth@viget.com>
2012-06-18 16:58:04 -0400

    fixed specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,22 +3,5 @@ FactoryGirl.define do
     email "user@email.com"
     password "pass"
   end
-
-  factory :badpass do
-    email "user@email.com"
-    password "bad"
-  end
-
-  factory :baduser do
-    email "bad@email.com"
-    password "pass"
-  end
-
-  factory :noemail do
-    password "pass"
-  end
-
-  factory :nopass do
-    email "user@email.com"
-  end
 end
+
