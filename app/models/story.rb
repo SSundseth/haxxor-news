daf1b46eec5edb09d948b6e8af08085d92ad8ed6
Stefan Sundseth <stefan.sundseth@viget.com>
2012-06-27 15:33:32 -0400

    added dependent => destroys

--- app/models/story.rb
+++ app/models/story.rb
@@ -8,7 +8,7 @@ class Story < ActiveRecord::Base
 
   belongs_to :user
   has_many :comments, :as => :commentable
-  has_many :votes, :as => :votable
+  has_many :votes, :as => :votable, :dependent => :destroy
 
   scope :chrono, :order => "created_at DESC"
 
