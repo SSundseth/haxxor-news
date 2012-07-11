eeb670d4175bc3d9eff40032941998d1088bb009
Stefan Sundseth <stefan.sundseth@viget.com>
2012-07-11 09:25:26 -0400

    changed 404 notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
   protect_from_forgery
+
+  def render_404
+    render :text => 'Not Found', :status => :not_found
+  end
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 end
